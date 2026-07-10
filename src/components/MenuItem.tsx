41e23335f7fa7d19dc6e2aa8bec8ab8b0937dacd
vachito <vacho19@gmail.com>
2026-07-09 23:27:31 -0600

    creado un custom hook para el state de ordenes y pasando una funcion de agregar orden desde el custom hook hacia menuitem

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -2,11 +2,13 @@ import type { MenuItem } from "../types"
 
 type MenuItemProps = {
     item:MenuItem    
+    addItem: () => void
 }
 
-export default function MenuItem({item}:MenuItemProps) {
+export default function MenuItem({item, addItem}:MenuItemProps) {
   return (
     <button
+        onClick={() => addItem}
         className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
     >
         <p>{item.name}</p>
