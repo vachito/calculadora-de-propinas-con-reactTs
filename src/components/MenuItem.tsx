e12fb8539cc1f7e7a103533185dbb437441e837f
vachito <vacho19@gmail.com>
2026-07-09 23:10:56 -0600

    se crea el header y un archivo json para mostrar los elementos del menu

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,16 @@
+import type { MenuItem } from "../types"
+
+type MenuItemProps = {
+    item:MenuItem    
+}
+
+export default function MenuItem({item}:MenuItemProps) {
+  return (
+    <button
+        className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
+    >
+        <p>{item.name}</p>
+        <p className=" font-black">Q {item.price}</p>
+    </button>
+  )
+}
